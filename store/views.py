71598300bb63112317df2b0960b647209dc7a80e
ectrodev <ectroshop9@gmail.com>
2026-04-20 11:54:39 +0200

    Update views.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product, Order, OrderItem
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductListAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.filter(is_available=True)
+        data = list(products.values('product_id', 'name', 'price', 'category', 'stock_quantity', 'image_url'))
+        return Response(data)
