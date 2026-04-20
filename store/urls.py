702a2200acd29c8f33edf70fa53ce06c4f4f8a66
ectrodev <ectroshop9@gmail.com>
2026-04-20 12:01:42 +0200

    Add new API routes for product and order management

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ProductListAPIView
+from . import views
 
 urlpatterns = [
-    path('api/products/', ProductListAPIView.as_view(), name='api_products'),
+    path('api/products/', views.ProductListAPIView.as_view(), name='api_products'),
+    path('api/products/<str:product_id>/', views.ProductDetailAPIView.as_view(), name='api_product_detail'),
+    path('api/orders/create/', views.CreateOrderAPIView.as_view(), name='api_create_order'),
+    path('api/orders/<str:user_id>/', views.OrderHistoryAPIView.as_view(), name='api_order_history'),
 ]
